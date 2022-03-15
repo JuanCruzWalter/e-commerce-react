7c54a0d2642f5a78dff08c547e0b798b4460d9dc
JuanCruzWalter <juancruzwalter@gmail.com>
2022-03-14 21:49:10 -0300

    creo el navBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
